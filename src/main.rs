aac41aac29e64722dc95000916750fa294335d63
netrolite <netrolite@proton.me>
2025-07-24 15:21:49 +0300

    recreate project with 2024 rust

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::{thread::sleep, time::Duration};
-
-pub fn main() {
-  let duration = Duration::from_secs(1);
-  let mut n = 5;
-
-  for _ in 0..100 {
-    n += 1;
-    println!("{}", n);
-    sleep(duration);
-  }
+fn main() {
+    println!("Hello, world!");
 }
