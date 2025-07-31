4aa6d3e94d73dea530912d4981b5140912414e03
netrolite <netrolite@proton.me>
2025-08-01 02:50:20 +0300

    program to find the nth fibonacci number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the nth fibonacci number you want to generate");
+    let mut seq_num = String::new();
+    io::stdin()
+        .read_line(&mut seq_num)
+        .expect("Could not read line");
+
+    let seq_num: u32 = seq_num.trim().parse().expect("Not a u32 value");
+    let fib = gen_fib_num(seq_num);
+    println!("Result: {fib}");
+}
+
+// 1 1 2 3 5 8 13
+fn gen_fib_num(seq_num: u32) -> u64 {
+    if seq_num <= 2 {
+        return 1;
+    }
+    return gen_fib_num(seq_num - 2) + gen_fib_num(seq_num - 1);
 }
