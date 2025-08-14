f9798120e319cdd9db4a8cfb9a3fd6fcdb25a275
netrolite <netrolite@proton.me>
2025-08-14 19:25:25 +0300

    graphemes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 fn main() {
-    #[derive(Debug)]
-    enum Cell {
-        Text(String),
-        Int(i32),
-        BigInt(i64),
-        Float(f64),
+    let s1 = String::from("helloйd");
+
+    for c in s1.graphemes(true) {
+        println!("{c}");
     }
-
-    let mut row = vec![
-        Cell::Text(String::from("my text")),
-        Cell::BigInt(30945782345087),
-    ];
-
-    if true {
-        row.push(Cell::Float(10.0));
-        row.push(Cell::Int(10));
-    }
-
-    for el in &row {
-        if let Cell::BigInt(val) = el {
-            println!("The element is a big ass integer: {val}!");
-        } else {
-            println!("The element is something other than a big ass integer!");
-        }
-    }
-
-    dbg!(row);
 }
