434ce4fd6012bd89b3be6d8e8910472c74a565ed
netrolite <netrolite@proton.me>
2025-08-15 00:49:29 +0300

    handling os errors

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,25 @@
-use std::collections::HashMap;
+use std::{fs::File, io::ErrorKind};
 
 fn main() {
-    let mut db = HashMap::new();
+    let file_path = "Cargo.tomll";
+    read_file_or_create_if_not_found(file_path);
+}
 
-    let blue_team_init_score = 0;
-    let red_team_init_score = 10;
-    let blue = String::from("blue");
-    let red = String::from("red");
-
-    db.insert(&blue, blue_team_init_score);
-    db.insert(&red, red_team_init_score);
-
-    let db_response = match db.get(&red) {
-        Some(val) => format!("Found value: {val}"),
-        None => String::from("Found nothin'"),
+fn read_file_or_create_if_not_found(file_path: &str) {
+    match File::open(file_path) {
+        Ok(_) => println!("Found file {file_path}"),
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => {
+                println!("File {file_path} not found");
+                println!("Attempting to create file named {file_path}");
+                match File::create(file_path) {
+                    Ok(_) => read_file_or_create_if_not_found(file_path),
+                    Err(err) => println!("Failed to create file. Error: {err}"),
+                }
+            }
+            err => {
+                println!("Unknown error when reading file: {err}")
+            }
+        },
     };
-    println!("db response: {db_response:?}");
-
-    println!("Iterating over HashMap before update:");
-    for (key, val) in &db {
-        println!("{key}: {val}");
-    }
-
-    let prev_red = *db.get(&red).unwrap_or(&0);
-    let prev_blue = *db.get(&blue).unwrap_or(&0);
-    db.insert(&red, prev_red + 10);
-    db.insert(&red, prev_blue + 10);
-
-    println!("Iterating over HashMap after update:");
-    for (key, val) in &db {
-        println!("{key}: {val}");
-    }
-
-    let key1 = String::from("red");
-    let key2 = String::from("sdf");
-
-    let inserted_value1 = db.entry(&key1).or_insert(70);
-    println!("first insertion result: {inserted_value1}");
-
-    let inserted_value2 = db.entry(&key2).or_insert(70);
-    println!("second insertion result: {inserted_value2}");
-
-    println!("db: {db:?}");
 }
